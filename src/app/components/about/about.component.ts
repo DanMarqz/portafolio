d62cd23facbb4ac8421a140541a1aac3dd1432da
Daniel <danielarturo.marquez@gmail.com>
2021-01-21 21:56:37 -0400

    maquetación about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  public title: string;
+  public subtitle: string;
+  public email: string;
 
-  constructor() { }
+  constructor() {
+    this.title = "Daniel Marquez";
+    this.subtitle = "Desarrollador Web";
+    this.email = "danielarturo.marquez@gmail.com";
+  }
 
   ngOnInit(): void {
   }
