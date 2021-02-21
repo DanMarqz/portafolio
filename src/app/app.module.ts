70277e890096befdb39f89f607dde8e9d100e4a1
Daniel <danielarturo.marquez@gmail.com>
2021-02-20 23:22:52 -0400

    act

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { CreateComponent } from './components/create/create.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ErrorComponent } from './components/error/error.component';
+import { DetailComponent } from './components/detail/detail.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ErrorComponent } from './components/error/error.component';
     CreateComponent,
     ProjectsComponent,
     ContactComponent,
-    ErrorComponent
+    ErrorComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,6 +6,8 @@ import { CreateComponent } from './components/create/create.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ErrorComponent } from './components/error/error.component';
+import { DetailComponent } from './components/detail/detail.component';
+
 
 const appRoutes: Routes = [
   {path: '', component: AboutComponent},
@@ -13,6 +15,7 @@ const appRoutes: Routes = [
   {path: 'proyectos', component:ProjectsComponent},
   {path: 'crear-proyecto', component:CreateComponent},
   {path: 'contacto', component:ContactComponent},
+  {path: 'proyecto/:id', component:DetailComponent},
   {path: '**', component: ErrorComponent}
 ];
 
